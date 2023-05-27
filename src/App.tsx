a73164a7ae072743147606844f447300e7258898
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-05-27 09:48:39 +0100

    💄 Add tailwind css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
-
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div class={'container mx-auto'}>
+      <header>
+        <h1 class={'text-5xl font-black text-center'}>Pathfinder app</h1>
       </header>
     </div>
   );
