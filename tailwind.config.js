b1e13d63ed01a331e06db04216f555640107d563
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-05-27 10:28:30 +0100

    🎨 Add eslint.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.tsx",
+    './src/**/*.tsx',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
