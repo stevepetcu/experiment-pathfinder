b2226c2d6af7efaf23c0505946d4a90fd1cdce99
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-06-15 21:55:50 +0100

    ✨ Configure the rate limiting params w/ env vars.

--- serverless-bff/utils/rate-limiter.ts
+++ serverless-bff/utils/rate-limiter.ts
@@ -1,20 +1,24 @@
 import {VercelRequest, VercelResponse} from '@vercel/node';
 import console from 'console';
 import limiter from 'lambda-rate-limiter';
+import * as process from 'process';
 
-const TEN_MINUTES = 600000;
+const TEN_MINUTES = '600000';
 
 const rateLimiter = limiter({
-  interval: TEN_MINUTES,
-  uniqueTokenPerInterval: 100, // I'm assuming this means "how many unique IPs we remember for the interval".
+  interval: parseInt(process.env.RATE_LIMIT_INTERVAL || TEN_MINUTES),
+  // I'm assuming this means "how many unique IPs we remember for the interval":
+  uniqueTokenPerInterval: parseInt(process.env.RATE_LIMIT_INTERVAL || '100'),
 });
 
 export const rateLimit = (fn: (request: VercelRequest, response: VercelResponse) => Promise<VercelResponse>) =>
   async (request: VercelRequest, response: VercelResponse): Promise<VercelResponse> => {
     try {
-      // Allow 30 requests per ten minutes from the same IP address.
-      // Which, if we have 1 OPTIONS request for each CORS request, should mean we can play 5 games in 10 min.
-      await rateLimiter.check(30, (request.headers['x-real-ip'] as unknown as string) || 'unknown');
+      // Allow 30 requests per ten minutes from a given IP address.
+      // Which, if we have 1 OPTIONS request for each CORS request,
+      // means we can play 5 games in 10 min. More, if CORS requests are cached.
+      await rateLimiter.check(parseInt(process.env.RATE_LIMIT_REQUESTS_PER_INTERVAL || '30'),
+        (request.headers['x-real-ip'] as unknown as string) || 'unknown');
     } catch {
       console.info('Rate limiting request.');
       return response.status(429).send(null);
