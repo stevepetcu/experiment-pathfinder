55fd0c302db1a27d4e17ba63ff8ce786af7e3b33
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-06-22 16:58:01 +0100

    💚 Add a test to the edge bff.

---
+++ edge-bff/tests/utils/fix-edge-uuid-format.test.ts
@@ -0,0 +1,17 @@
+import {describe, expect, test} from '@jest/globals';
+
+import {fixEdgeUuidFormat} from '../../utils/fix-edge-uuid-format';
+
+describe('fix-edge-uuid-format', () => {
+  const properlyFormattedUUID = '53f43abd-ed3d-4f4f-14c3-b0f1b8e4aa5d';
+
+  test('returns a correctly formatted UUID when passed in an "edge-format UUID"', () => {
+    const badlyFormattedUUID = '53f43abd-ed3d-4f4f-14c3b0f1b8e4aa5d';
+
+    expect(fixEdgeUuidFormat(badlyFormattedUUID)).toBe(properlyFormattedUUID);
+  });
+
+  test('returns the value that was passed in when it was already formatted properly', () => {
+    expect(fixEdgeUuidFormat(properlyFormattedUUID)).toBe(properlyFormattedUUID);
+  });
+});
