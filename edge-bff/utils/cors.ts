3d09af45732b5b5228e0c15a4f1a678a12940cd8
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-07-29 13:13:35 +0100

    🚑️ Fix CORS.

--- edge-bff/utils/cors.ts
+++ edge-bff/utils/cors.ts
@@ -4,8 +4,12 @@ export const allowCors = (fn: (request: Request, event: never, responseOpts: Res
   async (request: Request, event: never, responseOpts: ResponseOptions): Promise<Response> => {
     const resOpts = responseOpts || defaultResponseOptions();
 
+    const requestOrigin = request.headers.get('origin') || '';
+    const allowedOrigins = (process.env.CORS_ALLOWED_ORIGINS || '').split(',');
+    const allowedOriginHeaderValue = allowedOrigins.includes(requestOrigin) ? requestOrigin : '';
+
     resOpts.headers.set('Access-Control-Allow-Credentials', 'true');
-    resOpts.headers.set('Access-Control-Allow-Origin', process.env.CORS_ALLOWED_ORIGINS || '');
+    resOpts.headers.set('Access-Control-Allow-Origin', allowedOriginHeaderValue);
     resOpts.headers.set('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,POST');
     resOpts.headers.set(
       'Access-Control-Allow-Headers',
